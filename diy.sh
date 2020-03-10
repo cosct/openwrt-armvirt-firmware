5b3e847e390f5c63045d2dc49c4b989335a8e731
cosct <26520783+cosct@users.noreply.github.com>
2020-03-10 15:11:45 +0800

    update diy.sh

--- diy.sh
+++ diy.sh
@@ -13,6 +13,7 @@ git clone https://github.com/vernesong/OpenClash ../diy/luci-app-OpenClash
 git clone https://github.com/jerrykuku/luci-app-vssr ../diy/luci-app-vssr
 git clone https://github.com/asushugo/luci-app-koolproxyR ../diy/luci-app-koolproxyR
 git clone https://github.com/cosct/luci-theme-rosy ../diy/luci-theme-rosy
+git clone https://github.com/jerrykuku/lua-maxminddb.git ../diy/lua-maxminddb
 
 echo '修改koolshareR Makefile'
 sed -i 's/arm)/aarch64)/g' ../diy/luci-app-koolproxyR/Makefile
