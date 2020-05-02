d390cf984018b5ffb1bcf2f97461fcb9decdcde8
cosct <26520783+cosct@users.noreply.github.com>
2020-05-02 11:10:59 +0800

    update

--- diy.sh
+++ diy.sh
@@ -7,10 +7,10 @@
 #=================================================
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-echo '下载ServerChan rosy主题'
+echo '下载ServerChan Openclash rosy主题'
 git clone https://github.com/tty228/luci-app-serverchan ../diy/luci-app-serverchan
+git clone https://github.com/vernesong/OpenClash ../diy/luci-app-OpenClash
 git clone https://github.com/cosct/luci-theme-rosy ../diy/luci-theme-rosy
-
 
 
 echo '集成diy目录'
